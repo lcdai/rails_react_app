f9cda23ae494e04bbd7669a96ff3bce7a07fe20c
Dai Luong <lcongdai@gmail.com>
2025-08-06 14:10:04 +0700

    Added the Vite React client app

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import PostsList from './features/posts/PostsList.jsx'
+
+function App() {
+  return <>
+    <div className='app'>
+      <h1>React on Rails Blog</h1>
+      <p>Find this application layout in /client/src/App.jsx</p>
+      <PostsList />
+    </div>
+  </>
+}
+
+export default App
